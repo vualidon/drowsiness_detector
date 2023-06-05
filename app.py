d0e9ec9c94434c9610642b6f7189f4c227ecdd49
vualidon <92559181+vualidon@users.noreply.github.com>
2023-06-05 14:42:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ face_mesh2 = mp_face_mesh2.FaceMesh(min_detection_confidence=0.5, min_tracking_c
 mp_hand = mp.solutions.hands
 hands = mp_hand.Hands(
 model_complexity=0,
-min_detection_confidence=0.7,
-min_tracking_confidence=0.7
+min_detection_confidence=0.55,
+min_tracking_confidence=0.55
 )
 
 
